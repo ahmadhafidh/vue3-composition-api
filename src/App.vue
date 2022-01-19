2199e3a7cb4d5751340af9f4482b688e2ddffeba
ahmadhafidh <ahmad.hafidhayatullah@gmail.com>
2022-01-19 21:09:58 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Luas Segitiga</h1>
+  <Form @form="hitungLuas($event)" />
+  <h3> Luas</h3>
+  <h1> {{ luas }} </h1>
+  <Summary :alas="alas" :tinggi="tinggi" :luas="luas" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import Summary from './components/Summary.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  components:{
+    Summary,
+    Form
+  },
+  setup(){
+    const alas = ref(0);
+    const tinggi = ref(0);
+    const luas = ref(0);
+
+    function hitungLuas(event){
+      luas.value = parseFloat(event.alas) * parseFloat(event.tinggi) * 0.5;
+      alas.value = event.alas;
+      tinggi.value = event.tinggi;
+    }
+
+    return {
+      alas,
+      tinggi,
+      luas,
+      hitungLuas
+    }
   }
+
+
 }
 </script>
 
---
+++ src/components/Summary.vue
@@ -0,0 +1,19 @@
+<template>
+  <h4>
+    Luas dari segitiga yang memiliki alas {{ alas }} dan tinggi {{tinggi}} adalah {{luas}}
+  </h4>
+</template>
+
+<script>
+export default {
+     props: {
+         alas: Number,
+         tinggi: Number,
+         luas: Number,
+     }
+}
+</script>
+
+<style>
+
+</style>
