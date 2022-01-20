3d47951654f582767b5ad1b3df6ef7eb92971989
azadra527@gmail.com <azadra527@gmail.com>
2022-01-20 11:34:13 +0700

    luas dan keliling bangun datar

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,16 @@
 <template>
-  <h1> Luas Segitiga</h1>
-  <Form @form="hitungLuas($event)" />
-  <h3> Luas</h3>
-  <h1> {{ luas }} </h1>
-  <Summary :alas="alas" :tinggi="tinggi" :luas="luas" />
+  <header class="bg-gradient-to-r from-blue-500 to-gray-500">
+    <h1 class="text-gray-700 font-bold text-4xl py-3 px-5">Composition Api</h1>
+  </header>
+
+  <div class="flex flex-col min-h-screen bg-gradient-to-r from-blue-400 to-gray-400">
+    <router-view class="w-3/5 mx-auto my-4 text-center"/>
+    <nav class="font-bold text-gray-900 mx-auto my-3 text-xl">
+      <router-link class="opacity-50 hover:opacity-100" to="/">Segitiga</router-link> |
+      <router-link class="opacity-50 hover:opacity-100" to="/belah-ketupat">Belah Ketupat</router-link> |
+      <router-link class="opacity-50 hover:opacity-100" to="/trapesium">Trapesium</router-link> |
+      <router-link class="opacity-50 hover:opacity-100" to="/jajar-genjang">Jajar Genjang</router-link>
+    </nav>
+  </div>
+
 </template>
-
-<script>
-import { ref } from 'vue'
-import Summary from './components/Summary.vue'
-import Form from './components/Form.vue'
-
-export default {
-  name: 'App',
-
-  components:{
-    Summary,
-    Form
-  },
-  setup(){
-    const alas = ref(0);
-    const tinggi = ref(0);
-    const luas = ref(0);
-
-    function hitungLuas(event){
-      luas.value = parseFloat(event.alas) * parseFloat(event.tinggi) * 0.5;
-      alas.value = event.alas;
-      tinggi.value = event.tinggi;
-    }
-
-    return {
-      alas,
-      tinggi,
-      luas,
-      hitungLuas
-    }
-  }
-
-
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Segitiga from '../views/Segitiga.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Segitiga',
+    component: Segitiga
+  },
+  {
+    path: '/belah-ketupat',
+    name: 'Belah-Ketupat',
+    component: () => import('../views/Belah-Ketupat.vue')
+  }
+  ,
+  {
+    path: '/trapesium',
+    name: 'Trapesium',
+    component: () => import('../views/Trapesium.vue')
+  }
+  ,
+  {
+    path: '/jajar-genjang',
+    name: 'Jajar-Genjang',
+    component: () => import('../views/Jajar-Genjang.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
